4dae2b1e462eeaadf96383b72f6136b3d783f41f
Fumiya-Kito <cfbc.pearl@icloud.com>
2024-02-27 22:38:18 +0900

    chore: remove console logging

--- app/api/monos/route.ts
+++ app/api/monos/route.ts
@@ -31,8 +31,6 @@ async function GET(request: NextRequest) {
     const findCondition = categoryId ? { "category_id": +categoryId } : {};
 
     const response = await db.collection("monos").find(findCondition).toArray();
-    console.log('con==================', findCondition);
-    console.log('res==================', response);
     client.close();
     return NextResponse.json(response, {status: 200 });
   } catch (err) {
