ca75e97b2b5c17cbef05a124ac4dbabf0b91b834
Fumiya-Kito <cfbc.pearl@icloud.com>
2024-02-28 21:12:54 +0900

    chore: delete pageTitle

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Menu from "./Menu";
 import { CiSearch } from "react-icons/ci";
-import PageTitle from "./PageTitle";
+// import PageTitle from "./PageTitle";
 
 function Header() {
   return (
     <header className="p-6 bg-gray-800">
       <Menu />
-      <PageTitle />
+      {/* <PageTitle /> */}
 
       <div>
         <Link href="/search"><CiSearch /></Link>
