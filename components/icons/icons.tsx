b432df314963e495eae4ed4b003c5cdb96c97c31
Fumiya-Kito <cfbc.pearl@icloud.com>
2024-03-31 18:57:51 +0900

    feat: add icons ver2

--- components/icons/icons.tsx
+++ components/icons/icons.tsx
@@ -1,3 +1,4 @@
+import { MdImageNotSupported } from "react-icons/md";
 import { HiOutlineBattery0 } from "react-icons/hi2";
 import { HiOutlineComputerDesktop } from "react-icons/hi2";
 import { HiOutlineCake } from "react-icons/hi2";
@@ -25,6 +26,7 @@ import {
   IoShirtOutline,
   IoDocumentOutline,
   IoBedOutline,
+  IoLaptopOutline,
 } from "react-icons/io5";
 import {
   CiTablets1,
@@ -48,6 +50,7 @@ import { VscBlank } from "react-icons/vsc";
 import { BiBlanket } from "react-icons/bi";
 
 export const iconFormOptions = [
+  { value: "notsupported", label: <MdImageNotSupported color={"#000"} /> },
   { value: "battery", label: <HiOutlineBattery0 color={"#000"} /> },
   { value: "desktop", label: <HiOutlineComputerDesktop color={"#000"} /> },
   { value: "cake", label: <HiOutlineCake color={"#000"} /> },
@@ -63,14 +66,45 @@ export const iconFormOptions = [
   { value: "medicines", label: <GiMedicines color={"#000"} /> },
   { value: "towel", label: <GiTowel color={"#000"} /> },
   { value: "table", label: <GiTable color={"#000"} /> },
+  { value: "game", label: <IoGameControllerOutline color={"#000"} /> },
+  { value: "fastfood", label: <IoFastFoodOutline color={"#000"} /> },
+  { value: "phone", label: <IoPhonePortraitOutline color={"#000"} /> },
+  { value: "key", label: <IoKeyOutline color={"#000"} /> },
+  { value: "shirt", label: <IoShirtOutline color={"#000"} /> },
+  { value: "document", label: <IoDocumentOutline color={"#000"} /> },
+  { value: "bed", label: <IoBedOutline color={"#000"} /> },
+  { value: "laptop", label: <IoLaptopOutline color={"#000"} /> },
+  { value: "tablet", label: <CiTablets1 color={"#000"} /> },
+  { value: "keybord", label: <CiKeyboard color={"#000"} /> },
+  { value: "mouse", label: <CiDesktopMouse1 color={"#000"} /> },
+  { value: "creditcard", label: <CiCreditCard1 color={"#000"} /> },
+  { value: "glass", label: <CiGlass color={"#000"} /> },
+  { value: "screentablet", label: <SlScreenTablet color={"#000"} /> },
+  { value: "earphones", label: <SlEarphones color={"#000"} /> },
+  { value: "eyeglass", label: <SlEyeglass color={"#000"} /> },
+  { value: "bowl", label: <CgBowl color={"#000"} /> },
+  { value: "display", label: <FaDisplay color={"#000"} /> },
+  { value: "washdry", label: <TbWashDry2 color={"#000"} /> },
+  { value: "addresscard", label: <FaAddressCard color={"#000"} /> },
+  { value: "car", label: <LuCar color={"#000"} /> },
+  { value: "refrigerator", label: <LuRefrigerator color={"#000"} /> },
+  { value: "baguette", label: <TbBaguette color={"#000"} /> },
+  { value: "shoe", label: <TbShoe color={"#000"} /> },
+  { value: "backpack", label: <BsBackpack2 color={"#000"} /> },
+  { value: "spray", label: <FaSprayCanSparkles color={"#000"} /> },
+  { value: "spraycan", label: <LiaSprayCanSolid color={"#000"} /> },
+  { value: "pencilruler", label: <RiPencilRuler2Line color={"#000"} /> },
+  { value: "blank", label: <VscBlank color={"#000"} /> },
 ];
 
 export function getIconByString(target: string, size?: number) {
   const allIcons = [
+    { key: "notsupported", jsx: <MdImageNotSupported size={size} /> },
     { key: "battery", jsx: <HiOutlineBattery0 size={size} /> },
     { key: "desktop", jsx: <HiOutlineComputerDesktop size={size} /> },
     { key: "cake", jsx: <HiOutlineCake size={size} /> },
-    { key: "bag", jsx: <PiBowlFood size={size} /> },
+    { key: "bowlfood", jsx: <PiBowlFood size={size} /> },
+    { key: "bag", jsx: <PiBagLight size={size} /> },
     { key: "carrot", jsx: <PiCarrotLight size={size} /> },
     { key: "pants", jsx: <PiPantsLight size={size} /> },
     { key: "hanger", jsx: <PiCoatHangerLight size={size} /> },
@@ -81,6 +115,35 @@ export function getIconByString(target: string, size?: number) {
     { key: "medicines", jsx: <GiMedicines size={size} /> },
     { key: "towel", jsx: <GiTowel size={size} /> },
     { key: "table", jsx: <GiTable size={size} /> },
+    { key: "game", jsx: <IoGameControllerOutline size={size} /> },
+    { key: "fastfood", jsx: <IoFastFoodOutline size={size} /> },
+    { key: "phone", jsx: <IoPhonePortraitOutline size={size} /> },
+    { key: "key", jsx: <IoKeyOutline size={size} /> },
+    { key: "shirt", jsx: <IoShirtOutline size={size} /> },
+    { key: "document", jsx: <IoDocumentOutline size={size} /> },
+    { key: "bed", jsx: <IoBedOutline size={size} /> },
+    { key: "laptop", jsx: <IoLaptopOutline size={size} /> },
+    { key: "tablet", jsx: <CiTablets1 size={size} /> },
+    { key: "keybord", jsx: <CiKeyboard size={size} /> },
+    { key: "mouse", jsx: <CiDesktopMouse1 size={size} /> },
+    { key: "creditcard", jsx: <CiCreditCard1 size={size} /> },
+    { key: "glass", jsx: <CiGlass size={size} /> },
+    { key: "screentablet", jsx: <SlScreenTablet size={size} /> },
+    { key: "earphones", jsx: <SlEarphones size={size} /> },
+    { key: "bowl", jsx: <CgBowl size={size} /> },
+    { key: "display", jsx: <FaDisplay size={size} /> },
+    { key: "washdry", jsx: <TbWashDry2 size={size} /> },
+    { key: "addresscard", jsx: <FaAddressCard size={size} /> },
+    { key: "car", jsx: <LuCar size={size} /> },
+    { key: "refrigerator", jsx: <LuRefrigerator size={size} /> },
+    { key: "baguette", jsx: <TbBaguette size={size} /> },
+    { key: "shoe", jsx: <TbShoe size={size} /> },
+    { key: "backpack", jsx: <BsBackpack2 size={size} /> },
+    { key: "spray", jsx: <FaSprayCanSparkles size={size} /> },
+    { key: "spraycan", jsx: <LiaSprayCanSolid size={size} /> },
+    { key: "pencilruler", jsx: <RiPencilRuler2Line size={size} /> },
+    { key: "blank", jsx: <VscBlank size={size} /> },
+    { key: "blanket", jsx: <BiBlanket size={size} /> },
   ];
 
   return allIcons.find((icon) => icon.key === target)?.jsx;
